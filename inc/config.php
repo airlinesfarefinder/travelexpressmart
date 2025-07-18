25e0fe979e7d1e9fbe5f5e7016dac370e191d5a2
airlinesfarefinder <airlinesfarefinder@gmail.com>
2025-07-19 00:44:43 +0530

    update

--- inc/config.php
+++ inc/config.php
@@ -5,7 +5,7 @@ function config($key)
 {
     static $configuration = [
         'phone_number' => '(866) 337-7171',
-        'email_id' => 'info@demo.com',
+        'email_id' => 'info@travelexpressmart.com',
         'address' => 'demo, Boston, MA 02109, United States',
         'domain_name' => 'travelexpressmart.com',
     ];
